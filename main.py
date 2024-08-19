8e3104272dab56b9962b4b6186ce92e1bcfebe4f
MarekOstrowski <ostrowski.marek@boz.pl>
2024-08-19 12:12:19 +0200

    Add loop to iterate througt /Invoices and upload to ALL PDF's to IdosEll

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import requests
 from config import API_KEY
 from request_body import payload
 import pypdf
+import os
 
-order_number = 103640
 
 url = "https://butomaniak.pl/api/admin/v3/orders/documents"
 headers = {"accept": "application/json",
@@ -12,25 +12,27 @@ headers = {"accept": "application/json",
            "X-API-KEY": API_KEY}
 
 
-with open(f"invoices/{order_number}.pdf", "rb") as pdf_file:
-    reader = pypdf.PdfReader(pdf_file)
-    page1 = reader.get_page(0)
-    output_pdf = pypdf.PdfWriter()
-    output_pdf.add_page(page1)
-    output_pdf.write("first_page.pdf")
-    pdf1page = open("first_page.pdf", "rb")
-    encoded_string = base64.b64encode(pdf1page.read())
-    encoded_string = encoded_string.decode("ascii")
-    pdf1page.close()
+orders_list = list(map(lambda x: x.replace(".pdf", ""), os.listdir("./Invoices")))
+# print(orders_list)
 
+for order_number in orders_list:
 
-payload = payload(order_number=order_number, pdfBase64=encoded_string)
+    with open(f"invoices/{order_number}.pdf", "rb") as pdf_file:
+        reader = pypdf.PdfReader(pdf_file)
+        page1 = reader.get_page(0)
+        output_pdf = pypdf.PdfWriter()
+        output_pdf.add_page(page1)
+        output_pdf.write("first_page.pdf")
+        pdf1page = open("first_page.pdf", "rb")
+        encoded_string = base64.b64encode(pdf1page.read())
+        encoded_string = encoded_string.decode("ascii")
+        pdf1page.close()
 
+    request_data = payload(order_number=order_number, pdfBase64=encoded_string)
+    response = requests.post(url, json=request_data, headers=headers)
 
-response = requests.post(url, json=payload, headers=headers)
-
-print(f'URL: {response.request.url}')
-print(f'BODY: {response.request.body}')
-print(f'HEADERS: {response.request.headers}')
-print(f'RESPONSE: {response.text}')
-print(f'STATUS CODE: {response.status_code}')
+    print(f'URL: {response.request.url}')
+    # print(f'BODY: {response.request.body}')
+    print(f'HEADERS: {response.request.headers}')
+    print(f'RESPONSE: {response.text}')
+    print(f'STATUS CODE: {response.status_code}')
